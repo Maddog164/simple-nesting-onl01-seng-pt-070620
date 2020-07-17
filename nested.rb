e2693d703012b00399dd62fa91046f32c5abb7a4
Denise Henderson <denise@henderson3.us>
2020-07-17 01:45:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-
+		 programmer_hash[:yukihiro_matsumoto] =>{
+			 :known_for => "Ruby",
+			 :languagues => ["LISP", "C"]
+		 }
 end
 
 def changing_alan
